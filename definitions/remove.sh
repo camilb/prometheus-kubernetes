d167cdaf073913bd6724559476533c0fe2f88f2e
Camil Blanaru <camil@edka.io>
2016-11-16 13:26:26 +0100

    Add Kubernetes state metrics. Fix Grafana EC2 dashboard. Bump Prometehus to v1.3.1.

--- definitions/remove.sh
+++ definitions/remove.sh
@@ -3,4 +3,5 @@
 kubectl delete -f ./k8s/grafana
 kubectl delete -f ./k8s/ingress
 kubectl delete -f ./k8s/prometheus
+kubectl delete -f ./k8s/kube-state-metrics
 kubectl delete secret dhparam -n monitoring
