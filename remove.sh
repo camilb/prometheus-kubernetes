699cf2c95530ebbf4dbedd1ca158da165bd523f0
Camil Blanaru <camil@edka.io>
2017-06-15 17:06:01 +0200

    Fix RBAC config removal.

--- remove.sh
+++ remove.sh
@@ -4,6 +4,7 @@ kubectl delete -f ./k8s/grafana
 kubectl delete -R -f ./k8s/prometheus
 kubectl delete -f ./k8s/kube-state-metrics
 kubectl delete -f ./k8s/rbac/01-prometheus-rbac-config.yaml
+kubectl delete -f ./k8s/rbac/03-kube-state-metrics-rbac-config.yaml
 
 #Remove the Nginx Ingress Controller
 echo
