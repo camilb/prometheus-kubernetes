61526b760f61e0d23f7a741b871387d92d02c747
Valentino Pistis <vpistis@users.noreply.github.com>
2018-01-30 10:41:49 +0100

    Update generate-alertmanager-config-secret.sh
    fix AlertManager config map generation for macos when base64 command return multiline string

--- scripts/generate-alertmanager-config-secret.sh
+++ scripts/generate-alertmanager-config-secret.sh
@@ -6,6 +6,11 @@ else
   b64="base64 --wrap=0"
 fi
 
+# For MacOS that support --wrap=0 base64 return a multiline string
+if [[ $(cat ../assets/alertmanager/alertmanager.yaml | $b64 | wc -l) != 0 ]]; then
+    b64="base64 --wrap=0"
+fi
+
 cat <<-EOF >  ../manifests/alertmanager/alertmanager.cm.yaml
 apiVersion: v1
 kind: Secret
