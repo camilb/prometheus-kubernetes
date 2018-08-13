a28b919f85307f63a9673f32e5becf472c5b54fb
Camil Blanaru <camil@edka.io>
2018-08-13 22:31:46 +0200

    Fix namespace in scripts

--- tools/grafana_proxy.sh
+++ tools/grafana_proxy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#Namespace
+NAMESPACE=$(kubectl get sts --all-namespaces | grep prometheus-k8s | cut -d " " -f1)
+
 POD=$(kubectl get pods --namespace=$NAMESPACE | grep grafana| cut -d ' ' -f 1)
 kubectl port-forward $POD --namespace=$NAMESPACE 3000:3000
--- tools/prometheus_proxy.sh
+++ tools/prometheus_proxy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#Namespace
+NAMESPACE=$(kubectl get sts --all-namespaces | grep prometheus-k8s | cut -d " " -f1)
+
 POD=$(kubectl get pods --namespace=$NAMESPACE | grep prometheus-k8s-0| cut -d ' ' -f 1)
 kubectl port-forward $POD --namespace=$NAMESPACE 9090:9090
